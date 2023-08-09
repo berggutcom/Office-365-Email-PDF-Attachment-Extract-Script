a4e75c1b903f71936f737a3b1bf463fbd7ff9a98
Alex <51471724+berggutcom@users.noreply.github.com>
2023-08-09 13:09:50 +0200

    Update save_attachments.py

--- save_attachments.py
+++ save_attachments.py
@@ -11,13 +11,13 @@ import string
 
 # DO NOT FORGET TO CHANGE THESE VALUES
 # Of course the below ones are not valid
-TENANT_ID = '019c2xx0-c120-4a6a-9be5-39ed220a5875'
-CLIENT_ID = '0B0725d7-80ca-4acc-aba0-1821ea2ea4df'
-CLIENT_SECRET = 'A6Q8QUsK4pfdafEhGtsUh~z3trrQejlgjA4v'
-USER_ID = '402e16f2-1f22-4e5a-c1d9-91d924bde370'
-FOLDER_NAME = 'Accounting'
+TENANT_ID = '019c2xx0-c12xx0-4a6a-9be5-39ed220a5875'
+CLIENT_ID = '0B0725d7-80sca-4acc-aba0-1821ea2ea4df'
+CLIENT_SECRET = 'A6Q8QU234sK4pfdafEhGtsUh~z3trrQejlgjA4v'
+USER_ID = '402e16f2-1f22-4e5a-c1dss9-91d924bde370'
+FOLDER_NAME = 'Gesendete Elemente'
 FILE_EXTENSION = '.pdf'
-SAVE_PATH = '/Users/oz/Downloads/accounting-extract/'
+SAVE_PATH = '\Dokumente\Online_PDF'
 
 
 AUTHORITY = f'https://login.microsoftonline.com/{TENANT_ID}'
@@ -34,7 +34,7 @@ def authenticate():
     result = app.acquire_token_silent(SCOPES, account=None)
     if not result:
         result = app.acquire_token_for_client(scopes=SCOPES)
-    #print('success authenticating')
+    #print(result)
     return result['access_token']
 
 def get_emails():
@@ -44,16 +44,22 @@ def get_emails():
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json'
     }
+    #print(access_token)
     res_mailfolders = requests.get(f'{GRAPH_API_BASE_URL}users/{USER_ID}/mailFolders', headers=headers)
+    #print(res_mailfolders.json())
     for folder in res_mailfolders.json().get('value'):
         if folder["displayName"] == FOLDER_NAME:
             # Return the folder ID if the folder name matches
             FOLDER_ID = folder["id"]
     
     # Calculate date range
-    query = f"receivedDateTime ge 2022-01-01 and receivedDateTime le 2023-04-01 and hasAttachments eq true"
+    
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    query = f"receivedDateTime ge {today} and hasAttachments eq true"
+    
     # Get emails matching query
     url_to_loop = f'{GRAPH_API_BASE_URL}users/{USER_ID}/mailFolders/{FOLDER_ID}/messages?$filter={query}'
+    print(url_to_loop)
     while url_to_loop:
         response = requests.get(url_to_loop, headers=headers)
         data = response.json()
@@ -81,15 +87,19 @@ def save_attachments(email_id, attachment,email_date):
     filename_parts = os.path.splitext(attachment['name'])
     base_name = filename_parts[0]
     extension = filename_parts[1]
-    attachment_name = random_char + "_" + base_name[:48] + extension
+    attachment_name = base_name[:48] + extension
 
     attachment_content = attachment['contentBytes']
     attachment_data = base64.b64decode(attachment_content)
     
-    attachment_path = os.path.join(SAVE_PATH, f'{email_date}-{attachment_name}')
-    if attachment['contentType'] == f'application/{FILE_EXTENSION[1:]}':
-        with open(attachment_path, 'wb') as f:
-            f.write(attachment_data)
+    #check for filename
+    filename = attachment['name'].lower()
+    print(filename)
+    if filename.startswith('bestellbestätigung_') or filename.startswith('rechnung_'):
+        attachment_path = os.path.join(SAVE_PATH, f'{attachment_name}')
+        if attachment['contentType'] == f'application/{FILE_EXTENSION[1:]}':
+            with open(attachment_path, 'wb') as f:
+                f.write(attachment_data)
 
 def get_attachments(email):
     access_token = authenticate()
